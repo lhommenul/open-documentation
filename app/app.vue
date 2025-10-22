81f17bfe2826b72ff65e7ac50d6900b626478a95
Antoine Jézéquel <antoinejezequel@interstis.fr>
2025-10-22 13:01:11 +0200

    add description and favicon

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,13 @@
     <NuxtPage />
   </div>
 </template>
+
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Open Documentation',
+  description: 'Open documentation by Thirdshop',
+  ogTitle: 'Open Documentation',
+  ogDescription: 'Open documentation by Thirdshop',
+})
+</script>
